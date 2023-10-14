2ee3000b300c71afb9df621eb1caeb3042189edd
Abraham Olude <abraham.olude.ao@gmail.com>
2023-10-14 02:37:14 +0000

    Complete Mini Project: Grocery List

--- src/main/java/ReturnLongestString.java
+++ src/main/java/ReturnLongestString.java
@@ -8,6 +8,19 @@ public class ReturnLongestString {
      * @return the longest String within arr. You can assume that there are no ties for the longest string.
      */
     public String longest(String[] arr){
-        return null;
+
+        int longest = Integer.MIN_VALUE;
+        String longestString = "";
+
+        for(int i = 0; i < arr.length; i++) {
+            String currentString = arr[i];
+
+            if (currentString.length() > longest) {
+                longest = currentString.length();
+                longestString = currentString;
+            }
+        }
+
+        return longestString;
     }
 }
